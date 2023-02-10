70808c7f82e8377b32986109ba4de6962f577e15
sharonabrahamshaji <s222555241@deakin.edu.au>
2023-02-10 11:58:45 +1100

    Updates

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -37,19 +37,19 @@ const submitForm = () => {
         
     }
 
-const addCards = (items) => {
-    items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
-      '</div></div></div>';
-      $("#card-section").append(itemToAppend)
-    });
-}
+    const addCards = (items) => {
+        items.forEach(item => {
+            let itemToAppend = '<div class="col s4 center-align">'+
+        '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
+        '</div><div class="card-content">'+
+        '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right" more_vert></i></i></span><p><a href="#">'+item.link+'</a></p></div>'+
+        '<div class="card-reveal">'+
+            '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+            '<p class="card-text">'+item.description+'</p>'+
+          '</div></div></div>';
+          $("#card-section").append(itemToAppend)
+        });
+    }
 
 
 
